4e1e6cdcbf5cd15da9bc5c6006a669af0b35b218
Jay <jay.thakur.sdb@gmail.com>
2022-11-23 19:15:27 +0530

    added notification model

---
+++ db/migrate/20221123134000_create_notifications.rb
@@ -0,0 +1,12 @@
+class CreateNotifications < ActiveRecord::Migration[7.0]
+  def change
+    create_table :notifications do |t|
+      t.text :title
+      t.text :text
+      t.string :on_click_url
+      t.datetime :sent_at
+
+      t.timestamps
+    end
+  end
+end
